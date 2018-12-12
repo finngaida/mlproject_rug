304e7c68ba4c5c883f4439cf22f71669d9b724ac
s2589036 <nielsje@planet.nl>
2018-12-12 16:14:28 +0100

    makes separate files now

--- convert_yolodarknetdata.py
+++ convert_yolodarknetdata.py
@@ -9,7 +9,7 @@
 import csv
 
 #DIT HEEFT LENNART AL GEMAAKT, IK MOET IETS ANDERS DOEN, VRAGEN
-
+import pickle
 
 def classnumber(veh_class):
 	#print(veh_class)
@@ -18,8 +18,9 @@ def classnumber(veh_class):
 
 	return str(dictionary[veh_class])
 
-def main():
+def makedict():
 	file = open("gt_train.txt","r",encoding="UTF-16")
+	filedictionary = {}
 	for line in file.readlines():
 		params = line.rstrip("\n").split(",")
 		picture = params[0]
@@ -36,10 +37,27 @@ def main():
 		xmid = round((x1 + x2)/2 + 1)
 		ymid = round((y1 + y2)/2 + 1)
 
-		newline = type + " " + str(xmid) + " " + str(ymid) + " " + str(width) + " " + str(height)
-		print(newline)
+		newline = type + " " + str(xmid) + " " + str(ymid) + " " + str(width) + " " + str(height) + "\n"
+		if picture not in filedictionary:
+			filedictionary[picture] = [newline]
+		else:
+			filedictionary[picture].append(newline)
 
+	pickle.dump(filedictionary, open("filedictionary.pickle",'wb'))
+	print("Dict made")
 
+def main():
+	#makedict()
+	filedictionary = pickle.load(open("filedictionary.pickle","rb"))
+	for picture in filedictionary:
+		filename = picture+".txt"
+		file = open("nielsfiles/"+filename, "w")
+		for item in filedictionary[picture]:
+			if item != filedictionary[picture][-1]
+				file.write(item+"\n")
+			else:
+				file.write(item)
 
+		file.close()
 
 main()
