1db61d119dcb2fcf35c75a650936197f03586129
Finn Gaida <f@fga.pw>
2019-01-06 13:00:53 +0100

    update resize script with better parameter parsing

--- resize_images.py
+++ resize_images.py
@@ -1,46 +1,61 @@
 import sys
 import os
+import argparse
 from PIL import Image
 
-
-def resize_images(imgpath, sizes):
+'''
+Loops over the images in inpath and resizes all .jpg 's to the given size, then writes them to the outpath 
+'''
+def resize_images(inpath, outpath, sizes):
     # iterate over images and apply transform
-    for filename in os.listdir(imgpath):
+    for filename in os.listdir(inpath):
         # make sure we have an actual image
         filename_split = filename.split('.')
-        if filename_split[1] != 'jpg':
-            print('.{} is not a supported image format. Use \'.jpg\' instead.'.format(filename_split[1]))
+        if not len(filename_split) > 1 or filename_split[1] != 'jpg' and filename_split[1] != 'jpeg':
+            print('{} is not a supported image format, ignoring...'.format(filename))
             continue
 
         # do the resize
-        with open(imgpath+filename, 'r+b') as f:
+        with open(inpath+filename, 'r+b') as f:
             with Image.open(f) as image:
                 resized = image.resize((int(sizes[0]), int(sizes[1])))
-                # thumb = resizeimage.resize_thumbnail(image, [int(sizes[0]), int(sizes[1])])
-                resized.save(imgpath+'{}-resized.{}'.format(filename_split[0], filename_split[1]), image.format)
+                resized.save(outpath+filename, image.format)
 
-
-def main():
-    imgpath = ""
-    size = ""
-
-    # parse inputs
-    if len(sys.argv) < 5:
-        print('Usage: python3.5 resize_images.py -p <path to folder containing .jpg files> -s <\'width\'x\'height\'>\nexample: python3.5 resize_images.py -p \"MIO-TCD-Localization/train\" -s 600x600')
-        return
-
-    for i in range(1, len(sys.argv)):
-        if sys.argv[i] == "-p":
-            imgpath = sys.argv[i + 1]
-            i += 1
-        elif sys.argv[i] == "-s":
-            size = sys.argv[i + 1]
-            i += 1
-
-    # parse the size string
-    sizes = size.split('x')
-
-    resize_images(imgpath, sizes)
+# store the arguments
+FLAGS = None
 
 if __name__ == '__main__':
-    main()
+    intro_text = '''
+    Batch resize all the images in a given folder to the desired size
+    '''
+    parser = argparse.ArgumentParser(argument_default=argparse.SUPPRESS, description=intro_text)
+    '''
+    Command line options
+    '''
+    parser.add_argument(
+        'folder', type=str,
+        help='Folder in which the images are located'
+    )
+    parser.add_argument(
+        '-output', type=str, default='output/',
+        help='Folder in which output images should be stored'
+    )
+    parser.add_argument(
+        '-size', type=str,
+        help='Desired output size in the format like \'600x600\''
+    )
+
+    FLAGS = parser.parse_args()
+
+    # Create output folder only if it does not exist yet to prevent accidentally mixing test results
+    if not os.path.exists(FLAGS.output):
+        os.makedirs(FLAGS.output)
+
+    # Some people don't end paths with /
+    if FLAGS.folder[:-1] != '/':
+        FLAGS.folder = FLAGS.folder + '/'
+    if FLAGS.output[:-1] != '/':
+        FLAGS.output = FLAGS.output + '/'
+
+    # split the size into an array already
+    resize_images(FLAGS.folder, FLAGS.output, FLAGS.size.split('x'))
